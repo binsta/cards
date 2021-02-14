743aecbd908363d761a0c889a660ee331238974d
Binston-sukhael-cardoza <binstonsukhaelcardoza@outlook.com>
2021-02-14 18:58:24 +0530

    Solving tets array

--- lib/cards.ex
+++ lib/cards.ex
@@ -3,8 +3,10 @@ defmodule Cards do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamond"]
 
-    for suit <- suits do
-      suit
+    for values <- values do
+      for suit <- suits do
+        "#{value} of #{suit}"
+      end
     end
   end
   def shuffle(deck) do
